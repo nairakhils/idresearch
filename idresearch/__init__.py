31f52ca53ccef6a9bff6745c3b1635fec6479a35
nairakhils <nairakhilwork@gmail.com>
2022-06-25 21:42:34 +0530

    setup-related files

---
+++ idresearch/__init__.py
@@ -0,0 +1,7 @@
+from .idresearch import get_doc, get_ner, get_reco_df, main_abstract, reco_abstract, export_reco_csv, reco_authors, doc_stats, summarize_doc, plot_CitationCount_url, plot_CitationCount_df, plot_YearTrend_url, plot_YearTrend_df
+
+"""
+idresearch.
+
+idresearch is a package aimed to help researchers help identify relevant papers and see the development trend in that particular subfield.
+"""
